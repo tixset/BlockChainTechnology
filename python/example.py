17bcb9e9e9b0b466138413af07b7d4313ac0af99
Zelenov Anton <tixset@gmail.com>
2021-11-08 00:04:57 +0300

    Update example.py

--- python/example.py
+++ python/example.py
@@ -9,10 +9,10 @@ blockChain = [] # Объявляем массив блоков
 for i in range(0, 5): # Создаем 5 блоков
     blockChain = addBlock("Данные " + str(i), blockChain) # Создаем блок
     printBlock(i, blockChain) # Выводим данные
-    printVerify(blockChain) # Проверяем целосность цепочки
+    printVerify(blockChain) # Проверяем целостность цепочки
     print ""
 
 print "Измененные данные"
 blockChain[2].data = "Измененные данные" # Изменяем данные
 printBlock(2, blockChain) # Выводим данные
-printVerify(blockChain) # Проверяем целосность цепочки
+printVerify(blockChain) # Проверяем целостность цепочки
