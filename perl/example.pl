7ae6c69ef89232604b77870253c85430855522f7
Zelenov Anton <tixset@gmail.com>
2021-11-08 00:04:27 +0300

    Update example.pl

--- perl/example.pl
+++ perl/example.pl
@@ -7,11 +7,11 @@ my @blockChain; # Объявляем массив блоков
 for (my $i=0; $i < 5; $i++) { # Создаем 5 блоков
         @blockChain = block::addBlock("Данные $i", @blockChain); # Создаем блок
         block::printBlock($i, @blockChain); # Выводим данные
-        block::printVerify(@blockChain); # Проверяем целосность цепочки
+        block::printVerify(@blockChain); # Проверяем целостность цепочки
         print "\n";
 }
 
 print "Измененные данные\n";
 $blockChain[2]->{data} = "Измененные данные"; # Изменяем данные
 block::printBlock(2, @blockChain); # Выводим данные
-block::printVerify(@blockChain); # Проверяем целосность цепочки
+block::printVerify(@blockChain); # Проверяем целостность цепочки
