81c64788f73ad0d0b077edf1a8925e073d346b9c
Zelenov Anton <tixset@gmail.com>
2021-11-08 00:04:42 +0300

    Update example.php

--- php/example.php
+++ php/example.php
@@ -6,13 +6,13 @@ $blockChain = array(); // Объявляем массив блоков
 for ($i = 0; $i < 5; $i++) { // Создаем 5 блоков
 	$blockChain = addBlock("Данные $i", $blockChain); // Создаем блок
 	printBlock($i, $blockChain); // Выводим данные
-	printVerify($blockChain); // Проверяем целосность цепочки
+	printVerify($blockChain); // Проверяем целостность цепочки
 	echo "\n";
 }
 
 echo "Измененные данные\n";
 $blockChain[2]->data = "Измененные данные"; // Изменяем данные
 printBlock(2, $blockChain); // Выводим данные
-printVerify($blockChain); // Проверяем целосность цепочки
+printVerify($blockChain); // Проверяем целостность цепочки
 
 ?>
